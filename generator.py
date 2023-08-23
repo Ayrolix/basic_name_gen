debe08a60c394c3e996cc13a204330df8e8c184f
Cardinal DeVere <97072711+ImNarukami@users.noreply.github.com>
2023-08-23 18:01:22 -0500

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,16 @@
+import random
+
+hiragana = ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'を']
+romaji = ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'shi', 'su', 'se', 'so', 'ta', 'chi', 'tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho', 'ma', 'mi', 'mu', 'me', 'mo', 'ya', 'yu', 'yo', 'ra', 'ri', 'ru', 're', 'ro', 'wa', 'wo']
+final_hira = []
+final_roma = []
+
+def generate_name():
+    for x in range(random.randint(2, 6)):
+        choice = random.randint(0, len(hiragana) - 1)
+        final_hira.append(hiragana[choice])
+        final_roma.append(romaji[choice])
+        
+generate_name()
+print(final_hira)
+print(final_roma)
